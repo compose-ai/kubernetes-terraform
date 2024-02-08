f349ed9f2909cadcb7d0e13329f64c97bc5caf2c
Wilson Mitchell <github@wilsonmitchell.me>
2024-02-07 18:22:37 -0800

    Add output.

--- azurerm/_modules/aks/outputs.tf
+++ azurerm/_modules/aks/outputs.tf
@@ -10,3 +10,7 @@ output "kubeconfig" {
 output "default_ingress_ip" {
   value = length(azurerm_public_ip.current) > 0 ? azurerm_public_ip.current[0].ip_address : null
 }
+
+output "oidc_issuer_url" {
+  value = azurerm_kubernetes_cluster.current.oidc_issuer_url
+}

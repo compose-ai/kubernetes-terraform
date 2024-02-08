562ad13d773d2dd7b2191fd5f30e0382d3211b3d
Wilson Mitchell <github@wilsonmitchell.me>
2024-02-08 15:46:39 -0800

    Add required provider.

--- azurerm/_modules/aks/versions.tf
+++ azurerm/_modules/aks/versions.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "hashicorp/azuread"
       version = ">= 1.3.0"
     }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
   }
 
   required_version = ">= 0.13"
